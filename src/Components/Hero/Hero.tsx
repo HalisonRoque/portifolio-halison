258d5fd6743ff5b7be2a9f46fed320aa92f70afe
halisonyasha@gmail.com <halisonyasha@gmail.com>
2026-04-24 14:41:10 -0300

    criação de novo componeten para contar as expeiencias

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,10 +1,23 @@
 import styles from './styles.module.css';
 import { useTypewriter } from '../../hooks/useTypewriter';
 import HeroImage from './HeroImage';
+import { useEffect, useState } from 'react';
 
 export default function Hero() {
-
     const typedText = useTypewriter(["Node", "React", "TypeScript"]);
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkScreen = () => {
+            setIsMobile(window.innerWidth <= 500);
+        };
+
+        checkScreen(); // executa ao carregar
+        window.addEventListener("resize", checkScreen);
+
+        return () => window.removeEventListener("resize", checkScreen);
+    }, []);
 
     return (
         <section className={styles.section}>
@@ -15,13 +28,17 @@ export default function Hero() {
                         {typedText}
                     </span>
                 </h2>
+
                 <p className={styles.text}>
                     Transformando ideias em soluções digitais.
                 </p>
             </div>
-            <div className={styles.editImage}>
-                <HeroImage />
-            </div>
+
+            {!isMobile && (
+                <div className={styles.editImage}>
+                    <HeroImage />
+                </div>
+            )}
         </section>
-    )
+    );
 }
